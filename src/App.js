cb27e94555b5a0089a424d7b7beb7b0f248762fa
Maksim Golitsyn <maxim@infofree.ru>
2018-10-15 19:28:17 +0500

    finished challenge

--- src/App.js
+++ src/App.js
@@ -5,19 +5,65 @@ class App extends Component {
         super(props);
 
         this.state = {
-            counter: 0
+            error: '',
+            counter: 0,
         };
     }
 
+    showCountError = () => {
+        this.setState({
+            error: `The counter can't go below zero`
+        });
+    };
+
+    hideCountError = () => {
+        this.setState({ error: '' });
+    }
+
+    incrementCounter = () => {
+        if (this.state.error) {
+            this.hideCountError();
+        }
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    };
+
+    decrementCounter = () => {
+        // can't be below 0
+        if (this.state.counter === 0) {
+            this.showCountError();
+            return;
+        }
+
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    };
+
     render() {
+        const { error } = this.state;
+
         return (
             <div data-test="component-app">
                 <h1 data-test="counter-display">
                     The counter is currently { this.state.counter }
                 </h1>
+                {error && <div
+                    data-test="error-message"
+                    style={{ color: 'red' }}
+                >
+                    { error }
+                </div>}
                 <button
                     data-test="increment-button"
-                    onClick={ () => this.setState({counter: this.state.counter + 1}) }
+                    onClick={ this.incrementCounter }
+                >
+                    Increment counter
+                </button>
+                <button
+                    data-test="decrement-button"
+                    onClick={ this.decrementCounter }
                 >
                     Increment counter
                 </button>
